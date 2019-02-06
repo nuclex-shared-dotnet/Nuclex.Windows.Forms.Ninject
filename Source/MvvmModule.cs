863fff772638abf5ef15e4595ffaba4f7988600e
Markus Ewald <cygon@nuclex.org>
2019-02-06 18:41:58 +0000

    Added a separate assembly for the Ninject bindings because I do not wish to add Ninject as a core dependency (there are a ton of alternative dependency injectors out there!)
    git-svn-id: file:///srv/devel/repo-conversion/dumps/svni@1 d2e56fa2-650e-0410-a79f-9358c0239efd

---
+++ Source/MvvmModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Ninject;
+using Ninject.Activation;
+using Ninject.Modules;
+
+using Nuclex.Windows.Forms.AutoBinding;
+
+namespace Nuclex.Windows.Forms.Ninject {
+
+	/// <summary>Sets up the service bindings for an MVVM-based WPF application</summary>
+	public class MvvmModule : NinjectModule {
+
+		/// <summary>Called when the module is loaded into the kernel</summary>
+		public override void Load() {
+
+			// The window manager keeps track of active windows and can figure out
+			// which window to display for a view model by its naming convention.
+			Kernel.Bind<WindowManager>().To<NinjectWindowManager>().InSingletonScope();
+			Kernel.Bind<IWindowManager>().ToMethod(getWindowManager).InSingletonScope();
+			Kernel.Bind<IActiveWindowTracker>().ToMethod(getWindowManager).InSingletonScope();
+			Kernel.Bind<IAutoBinder>().ToMethod(CreateAutoBinder).InSingletonScope();
+
+		}
+
+		/// <summary>Creates and initializd the auto view model binder</summary>
+		/// <param name="context">
+		///   Context containing environmental informations about the request and the kernel
+		/// </param>
+		/// <returns>The view model auto binder that will be used by the application</returns>
+		protected virtual IAutoBinder CreateAutoBinder(IContext context) {
+			return new ConventionBinder();
+		}
+
+		/// <summary>Retrieves the window manager from the kernel</summary>
+		/// <param name="context">
+		///   Context containing environmental informations about the request and the kernel
+		/// </param>
+		/// <returns>The window manager registered to the kernel</returns>
+		private static WindowManager getWindowManager(IContext context) {
+			return context.Kernel.Get<WindowManager>();
+		}
+
+	}
+
+} // namespace Nuclex.Windows.Forms.Ninject
---
+++ Source/NinjectWindowManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Ninject;
+
+using Nuclex.Windows.Forms.AutoBinding;
+
+namespace Nuclex.Windows.Forms.Ninject {
+
+	/// <summary>Window manager that is using Ninject</summary>
+	public class NinjectWindowManager : WindowManager {
+
+		/// <summary>Initializes a new window manager</summary>
+		///	<param name="kernel">
+		///	  Ninject kernel the window manager uses to construct view models
+		///	</param>
+		/// <param name="autoBinder">
+		///   View model binder that will be used to bind all created views to their models
+		/// </param>
+		public NinjectWindowManager(IKernel kernel, IAutoBinder autoBinder = null) :
+			base(autoBinder) {
+			this.kernel = kernel;
+		}
+
+		/// <summary>Creates an instance of the specified type</summary>
+		/// <param name="type">Type an instance will be created of</param>
+		/// <returns>The created instance</returns>
+		/// <remarks>
+		///   Use this to wire up your dependency injection container. By default,
+		///   the Activator class will be used to create instances which only works
+		///   if all of your view models are concrete classes.
+		/// </remarks>
+		protected override object CreateInstance(Type type) {
+			return this.kernel.Get(type);
+		}
+
+		/// <summary>The Ninject kernel used to create new instances</summary>
+		private readonly IKernel kernel;
+
+	}
+
+} // namespace Nuclex.Windows.Forms.Ninject
